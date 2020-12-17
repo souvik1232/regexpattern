192fd64c81b9cfe07c3b2c16049c82d8bf7a8ed0
souvik1232 <souvik199831@gmail.com>
2020-12-17 21:36:03 +0530

    Added:Pattern for email validation

--- RegexPattern.java
+++ RegexPattern.java
@@ -9,6 +9,7 @@ public class RegexPattern {
         System.out.println("Welcome");
         String firstName;
         String lastName;
+        String email;
         String pattern;
         do {
             pattern="^[A-Z]{1}[a-z]{2,}$";
@@ -23,6 +24,13 @@ public class RegexPattern {
             lastName=scanner.next();
 
         }while(!Pattern.matches(pattern,lastName));
+        System.out.println("Valid");
+        do {
+            pattern="^[a-zA-Z0-9\\-\\+\\.]+.([a-zA-Z0-9])*@([a-z0-9]+.[a-z]{2,}.([a-z]{2,})?)$";
+            System.out.println("Enter a valid email");
+            email=scanner.next();
+
+        }while(!Pattern.matches(pattern,email));
         System.out.println("Valid");
     }
 }
