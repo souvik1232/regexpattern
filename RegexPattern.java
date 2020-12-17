c88808aa518e00bb8b3d5f9b6a31a2820cef9b9b
souvik1232 <souvik199831@gmail.com>
2020-12-17 21:11:41 +0530

    Added:Pattern for first name check

--- RegexPattern.java
+++ RegexPattern.java
@@ -7,14 +7,14 @@ public class RegexPattern {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome");
-//        String firstName;
-//        String pattern;
-//        do {
-//            pattern="^[A-Z]{1}[a-z]{2,}$";
-//            System.out.println("Enter a valid First Name");
-//            firstName=scanner.next();
-//
-//        }while(!Pattern.matches(pattern,firstName));
-//        System.out.println("Valid");
+        String firstName;
+        String pattern;
+        do {
+            pattern="^[A-Z]{1}[a-z]{2,}$";
+            System.out.println("Enter a valid First Name");
+            firstName=scanner.next();
+
+        }while(!Pattern.matches(pattern,firstName));
+        System.out.println("Valid");
     }
 }
