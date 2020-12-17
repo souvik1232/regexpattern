df54908780443bcfb25319fd5d25953533709b94
souvik1232 <souvik199831@gmail.com>
2020-12-17 21:09:32 +0530

    Added:Welcome message

---
+++ RegexPattern.java
@@ -0,0 +1,20 @@
+package com.java.regexpattern;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class RegexPattern {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome");
+//        String firstName;
+//        String pattern;
+//        do {
+//            pattern="^[A-Z]{1}[a-z]{2,}$";
+//            System.out.println("Enter a valid First Name");
+//            firstName=scanner.next();
+//
+//        }while(!Pattern.matches(pattern,firstName));
+//        System.out.println("Valid");
+    }
+}
